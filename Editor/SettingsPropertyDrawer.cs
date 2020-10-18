15d26abed148758f322a9300e0736559959a4e1c
Phill <philldamaskin@gmail.com>
2020-10-18 17:31:12 -0400

    clean up surrogate property code for the ui

--- Editor/SettingsPropertyDrawer.cs
+++ Editor/SettingsPropertyDrawer.cs
@@ -10,41 +10,55 @@ namespace Popcron.Settings
     public class SettingsPropertyDrawer : PropertyDrawer
     {
         private static string[] displayOptions = null;
-        private static FieldInfo[] supportedFields = null;
-        private static SerializedObject[] supportedTypesObjects = null;
+        private static FieldInfo[] surrogateFields = null;
 
         private static void FindAllTypes()
         {
             //add types from the supported types class
-            supportedFields = typeof(SupportedTypes).GetFields();
-            displayOptions = new string[supportedFields.Length];
-            supportedTypesObjects = new SerializedObject[supportedFields.Length];
+            surrogateFields = typeof(SupportedTypes).GetFields();
+            displayOptions = new string[surrogateFields.Length];
             for (int i = 0; i < displayOptions.Length; i++)
             {
-                SupportedTypes supportedTypes = ScriptableObject.CreateInstance<SupportedTypes>();
-                supportedTypesObjects[i] = new SerializedObject(supportedTypes);
-                displayOptions[i] = supportedFields[i].FieldType.Name;
+                displayOptions[i] = surrogateFields[i].FieldType.Name;
             }
         }
 
-        private FieldInfo GetField(SerializedProperty typeProperty)
+        /// <summary>
+        /// Returns the surrogate field based on this type property.
+        /// </summary>
+        private FieldInfo GetSurrogateField(SerializedProperty typeProperty)
         {
             int selectedIndex = 0;
-            for (int i = 0; i < supportedFields.Length; i++)
+            for (int i = 0; i < surrogateFields.Length; i++)
             {
-                if (supportedFields[i].FieldType.FullName == typeProperty.stringValue)
+                if (surrogateFields[i].FieldType.FullName == typeProperty.stringValue)
                 {
                     selectedIndex = i;
                     break;
                 }
             }
 
-            return supportedFields[selectedIndex];
+            return surrogateFields[selectedIndex];
+        }
+
+        private SerializedObject CreateSurrogateTypeObject(SerializedProperty typeProperty, SerializedProperty defaultValueProperty)
+        {
+            //inefficient? i know
+            //create the suty object
+            SupportedTypes supportedTypes = ScriptableObject.CreateInstance<SupportedTypes>();
+            SerializedObject serializedTypes = new SerializedObject(supportedTypes);
+            FieldInfo supportedTypeField = GetSurrogateField(typeProperty);
+
+            //load from json into the correct field
+            object objectValue = JsonConvert.DeserializeObject(defaultValueProperty.stringValue, supportedTypeField.FieldType);
+            supportedTypeField.SetValue(supportedTypes, objectValue);
+            serializedTypes.Update();
+            return serializedTypes;
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (supportedFields == null || displayOptions == null)
+            if (surrogateFields == null || displayOptions == null)
             {
                 FindAllTypes();
             }
@@ -53,39 +67,40 @@ namespace Popcron.Settings
             EditorGUI.PropertyField(position, property, label);
             if (property.isExpanded)
             {
-                SerializedProperty name = property.FindPropertyRelative(nameof(Property.name));
-                SerializedProperty type = property.FindPropertyRelative(nameof(Property.type));
-                SerializedProperty value = property.FindPropertyRelative(nameof(Property.defaultValue));
+                SerializedProperty nameProperty = property.FindPropertyRelative(nameof(Property.name));
+                SerializedProperty typeProperty = property.FindPropertyRelative(nameof(Property.type));
+                SerializedProperty defaultValueProperty = property.FindPropertyRelative(nameof(Property.defaultValue));
 
                 position.y += 20;
-                EditorGUI.PropertyField(position, name, new GUIContent("Name"));
+                EditorGUI.PropertyField(position, nameProperty, new GUIContent("Name"));
 
                 position.y += 20;
-                FieldInfo fieldInfo = ShowTypeEnum(position, type);
+                FieldInfo fieldInfo = ShowTypeEnum(position, typeProperty);
 
                 position.y += 20;
-                ShowDefaultValue(position, fieldInfo, value);
-
-                SettingsInspector.propertyIndex++;
+                SerializedObject surrogateTypesObject = CreateSurrogateTypeObject(typeProperty, defaultValueProperty);
+                ShowDefaultValue(position, fieldInfo, defaultValueProperty, surrogateTypesObject);
             }
         }
 
-        private SerializedProperty GetProperty(FieldInfo fieldInfo)
+        /// <summary>
+        /// Returns the serialized property that can represent this field info.
+        /// </summary>
+        private SerializedProperty GetSurrogateProperty(FieldInfo surrogateFieldInfo, SerializedObject surrogateTypesObject)
         {
             //grab a serialized object based on this field index
-            SerializedObject supportedTypesObject = supportedTypesObjects[SettingsInspector.propertyIndex];
-            SerializedProperty next = supportedTypesObject.FindProperty("first").Copy();
+            SerializedProperty next = surrogateTypesObject.FindProperty("first").Copy();
             next.Next(false);
             int index = 0;
             while (true)
             {
-                if (supportedFields[index] == fieldInfo)
+                if (surrogateFields[index] == surrogateFieldInfo)
                 {
                     return next.Copy();
                 }
 
                 index++;
-                if (index == supportedFields.Length)
+                if (index == surrogateFields.Length)
                 {
                     break;
                 }
@@ -98,22 +113,21 @@ namespace Popcron.Settings
             return null;
         }
 
-        private float GetHeight(FieldInfo fieldInfo)
+        private float GetHeight(FieldInfo fieldInfo, SerializedObject serializedTypes)
         {
             //grab a serialized object based on this field index
-            SerializedObject supportedTypesObject = supportedTypesObjects[0];
-            SerializedProperty next = supportedTypesObject.FindProperty("first").Copy();
+            SerializedProperty next = serializedTypes.FindProperty("first").Copy();
             next.Next(false);
             int index = 0;
             while (true)
             {
-                if (supportedFields[index] == fieldInfo)
+                if (surrogateFields[index] == fieldInfo)
                 {
                     return EditorGUI.GetPropertyHeight(next);
                 }
 
                 index++;
-                if (index == supportedFields.Length)
+                if (index == surrogateFields.Length)
                 {
                     break;
                 }
@@ -126,12 +140,15 @@ namespace Popcron.Settings
             return 0f;
         }
 
+        /// <summary>
+        /// Displays a type enum and returns the surrogate field that matches this type.
+        /// </summary>
         private FieldInfo ShowTypeEnum(Rect position, SerializedProperty typeProperty)
         {
             int selectedIndex = 0;
-            for (int i = 0; i < supportedFields.Length; i++)
+            for (int i = 0; i < surrogateFields.Length; i++)
             {
-                if (supportedFields[i].FieldType.FullName == typeProperty.stringValue)
+                if (surrogateFields[i].FieldType.FullName == typeProperty.stringValue)
                 {
                     selectedIndex = i;
                     break;
@@ -141,32 +158,26 @@ namespace Popcron.Settings
             int newIndex = EditorGUI.Popup(position, "Type", selectedIndex, displayOptions);
             if (newIndex != selectedIndex)
             {
-                Type systemType = supportedFields[newIndex].FieldType;
+                Type systemType = surrogateFields[newIndex].FieldType;
                 typeProperty.stringValue = systemType.FullName;
             }
 
-            return supportedFields[newIndex];
+            return surrogateFields[newIndex];
         }
 
-        private void ShowDefaultValue(Rect position, FieldInfo fieldInfo, SerializedProperty value)
+        private void ShowDefaultValue(Rect position, FieldInfo surrogateField, SerializedProperty defaultValueProperty, SerializedObject surrogateTypesObject)
         {
-            SerializedProperty property = GetProperty(fieldInfo);
-            SupportedTypes targetObject = (SupportedTypes)property.serializedObject.targetObject;
-            FieldInfo originalField = targetObject.GetType().GetField(property.name);
-
-            //load from json
-            object objectValue = JsonConvert.DeserializeObject(value.stringValue, originalField.FieldType);
-            originalField.SetValue(targetObject, objectValue);
-            property.serializedObject.Update();
+            SerializedProperty surrogateProperty = GetSurrogateProperty(surrogateField, surrogateTypesObject);
 
             //show property
-            EditorGUI.PropertyField(position, property, new GUIContent("Default"));
-            property.serializedObject.ApplyModifiedProperties();
+            EditorGUI.PropertyField(position, surrogateProperty, new GUIContent("Default"));
+            surrogateProperty.serializedObject.ApplyModifiedProperties();
 
-            //save to json
-            objectValue = originalField.GetValue(targetObject);
-            value.stringValue = JsonConvert.SerializeObject(objectValue);
-            value.serializedObject.ApplyModifiedProperties();
+            //save to json by getting the value from suty
+            //and then by writing back into the original property as a string
+            object objectValue = surrogateField.GetValue(surrogateTypesObject.targetObject);
+            defaultValueProperty.stringValue = JsonConvert.SerializeObject(objectValue);
+            defaultValueProperty.serializedObject.ApplyModifiedProperties();
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
@@ -175,8 +186,11 @@ namespace Popcron.Settings
             {
                 float height = 20f * 3f;
                 SerializedProperty typeProperty = property.FindPropertyRelative(nameof(Property.type));
-                FieldInfo propertyType = GetField(typeProperty);
-                return height + GetHeight(propertyType);
+                SerializedProperty defaultValueProperty = property.FindPropertyRelative(nameof(Property.defaultValue));
+                SerializedObject surrogateTypesObject = CreateSurrogateTypeObject(typeProperty, defaultValueProperty);
+                FieldInfo surrogateFieldInfo = GetSurrogateField(typeProperty);
+                SerializedProperty propertyType = GetSurrogateProperty(surrogateFieldInfo, surrogateTypesObject);
+                return height + EditorGUI.GetPropertyHeight(propertyType, true);
             }
             else
             {
